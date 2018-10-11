ee4a44790e4e3ed918bbb542c493b6e84806473f
Martin Hunt <martinhunt@iMac.local>
2018-10-11 16:25:15 -0700

    first queries

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if they are logged in!
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info) // info is passed as a second argument because it contains our request detauils which specify what gets returned from the server
+
+    return item
+  }
+};
+
+module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+  
+  /* Regular way to call the databsae query
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items()
+    return items
+  }
+  Or can use the below if it's just passing the query on with no custom logic*/ 
+  items: forwardTo('db')
+
+};
 
 module.exports = Query;
